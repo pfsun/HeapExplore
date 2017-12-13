a914102806685024b57718cb57fcd709c126fbe0
kamadanen <hcheerment@gmail.com>
2017-12-12 20:30:49 -0500

    Dec12th

--- tools/explore_data.py
+++ tools/explore_data.py
@@ -13,21 +13,23 @@ import data_visualization as dv
 import numpy as np
 import matplotlib.pyplot as plt
 import json as js
+import keras
 
 
 
 #%% read data
-f = open('../Data/data', 'r')
+f = open('../../Data/data', 'r')
 X = js.load(f)
 f.close()
 data_size = len(X) 
 #%%
-ll = []
-for sample in X:
-    ll.append(len(sample[0]))
-ll_hist = np.histogram(ll, bins=1000)
-plt.figure()
-plt.plot(ll_hist[1][:-1], ll_hist[0])
+def plotHist(X):
+    ll = []
+    for sample in X:
+        ll.append(len(sample[0]))
+    ll_hist = np.histogram(ll, bins=1000)
+    plt.figure()
+    plt.plot(ll_hist[1][:-1], ll_hist[0])
 
 #%% visualize data
 vasualization_idx = np.arange(9)
@@ -37,3 +39,25 @@ dv.plotData(vasualization_idx+offset, X)
 
 
 plt.show()
+#%%
+def extractDump(X, lower, upper):
+    X1 = []
+    for sample in X:
+        if len(sample[0]) < upper:
+            if len(sample[0]) > lower:
+                X1.append(sample)
+    return X1
+#%%
+max_len = 0
+for sample in X1:
+    if max_len < len(sample[0]):
+        max_len = len(sample[0])
+#%%
+#data = np.zeros([len(X1), max_len])
+X_train_list = []
+y_train_list = []
+for sample in X1:
+    X_train_list.append(sample[0])
+    y_train_list.append(sample[1])
+X_train = keras.preprocessing.sequence.pad_sequences(X_train_list, padding='post', value=0.)
+y_train = keras.preprocessing.sequence.pad_sequences(y_train_list, padding='post', value=0.)
